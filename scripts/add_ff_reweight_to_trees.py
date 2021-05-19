31ab08d6707131254eaa78b5cddba31cca86b65f
George <gputtley@hotmail.com>
2021-05-19 16:42:13 +0100

    Adding scripts to add output to trees

--- scripts/add_ff_reweight_to_trees.py
+++ scripts/add_ff_reweight_to_trees.py
@@ -1,55 +1,81 @@
 import uproot
+import os
+import json
 import pandas
 import pickle
-from UserCode.ICHiggsTauTau.ff_ml_tools import SelectColumns,to_root
+import argparse
+from UserCode.ReweightedFakeFactors.ff_ml_tools import SelectColumns,to_root
 
-loc = "/vols/cms/gu18/Offline/output/MSSM/mssm_2017_v10"
-newloc = "./"
-file_name = "SingleMuonD"
+# python scripts/add_ff_reweight_to_trees.py --input_location=/vols/cms/gu18/Offline/output/MSSM/mssm_2017_v10 --output_location=/vols/cms/gu18/Offline/output/MSSM/mssm_2017_v10_new --filename=SingleMuonB_mt_2017.root --channel=mt --year=2017 --splitting=100000 --offset=0
 
-columns = ["pt_1","pt_2","jet_pt_2","jet_pt_2/pt_2","n_jets","n_deepbjets","tau_decay_mode_2","trg_etaucross","dR","dphi","pt_1*cosh(eta_1)","pt_2*cosh(eta_2)","met","rho"]
-#columns = ["trg_mutaucross","dphi","mva_dm_2","pt_2","pt_1","met","rho","n_jets","jet_pt_2","dR","n_deepbjets","pt_2*cosh(eta_2)","pt_1*cosh(eta_1)","jet_pt_2/pt_2"] 
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_location',help= 'Name of input location (not including file name)', default='/vols/cms/gu18/Offline/output/MSSM/mssm_2017_v10')
+parser.add_argument('--output_location',help= 'Name of output location (not including file name)', default='./')
+parser.add_argument('--filename',help= 'Name of file', default='SingleMuonB_mt_2017.root')
+parser.add_argument('--channel',help= 'Name of channel', default='mt')
+parser.add_argument('--year',help= 'Name of year', default='2017')
+parser.add_argument('--splitting',help= 'Number of events per task', default='100000')
+parser.add_argument('--offset',help= 'Offset of job', default='0')
+args = parser.parse_args()
+
+columns = {"et":["pt_1","pt_2","jet_pt_2","jet_pt_2/pt_2","n_jets","n_deepbjets","tau_decay_mode_2","trg_etaucross","dR","dphi","pt_1*cosh(eta_1)","pt_2*cosh(eta_2)","met","rho"],
+           "mt":["pt_1","pt_2","jet_pt_2","jet_pt_2/pt_2","n_jets","n_deepbjets","tau_decay_mode_2","trg_etaucross","dR","dphi","pt_1*cosh(eta_1)","pt_2*cosh(eta_2)","met","rho"],
+           "tt":[]}
+
 weights = ["wt","wt_tau_trg_mssm","wt_tau_id_mssm"]
 
-channel = "mt"
-year = "2017"
-file_name = "{}_{}_{}.root".format(file_name,channel,year)
+json_name = 'BDTs/ff_reweight_normalisation.json'
+with open(json_name) as json_file:
+  norm_dict = json.load(json_file)
 
 
-norm = {"et": {"2016": {"qcd":0.17, "wjets":0.17, "ttbar":0.17}, "2017":{"qcd":0.131450286969, "wjets":0.254104017874, "ttbar":0.130039812167}, "2018":{"qcd":0.17, "wjets":0.17, "ttbar":0.17}},
-        "mt": {"2016":{"qcd":0.17, "wjets":0.17, "ttbar":0.17}, "2017":{"qcd":0.17, "wjets":0.17, "ttbar":0.17}, "2018":{"qcd":0.17, "wjets":0.17, "ttbar":0.17}}}
+tree = uproot.open(args.input_location+'/'+args.filename)["ntuple"]
 
-tree = uproot.open(loc+'/'+file_name)["ntuple"]
-j = 0
-for small_tree in tree.iterate(entrysteps=10000):
-  print j
-  #df = small_tree.pandas.df(tree.keys())
-  df = pandas.DataFrame.from_dict(small_tree)
+k = 0
+for small_tree in tree.iterate(entrysteps=int(args.splitting)):
+  print k,int(args.offset)
+  if k == int(args.offset):
+    print k
+    df = pandas.DataFrame.from_dict(small_tree)
+    
+    for i in weights:
+      if i == weights[0]:
+        total_weights = df.loc[:,i]
+      else:
+        total_weights = total_weights.multiply(df.loc[:,i])
+
+    new_df = SelectColumns(df,columns[args.channel])
+
+    if args.channel in ["mt","et"]:
+
+      wjets_reweighter = pickle.load(open("BDTs/wjets_mc_reweighted_ff_{}_{}.sav".format(args.channel,args.year), 'rb'))
+      wjets_out = wjets_reweighter.predict_weights(new_df,total_weights)
+      df.loc[:,"wt_ff_reweight_wjets_mc_1"] = wjets_out*norm_dict[args.channel][args.year]["wjets_mc"]
   
-  for i in weights:
-    if i == weights[0]:
-      total_weights = df.loc[:,i]
-    else:
-      total_weights = total_weights.multiply(df.loc[:,i])
-  
-  new_df = SelectColumns(df,columns)
-  
-  
-  wjets_reweighter = pickle.load(open("wjets_mc_reweighted_ff_{}_{}.sav".format(channel,year), 'rb'))
-  wjets_out = wjets_reweighter.predict_weights(new_df,total_weights)
-  df.loc[:,"wt_ff_reweight_wjets_1"] = wjets_out*norm[channel][year]["wjets"]
-  
-  #print df.loc[:,["wt_ff_reweight_wjets_1","wt_ff_mssm_wjets_1","jet_pt_2","tau_decay_mode_2","mva_dm_2"]]
-  
-  #qcd_reweighter = pickle.load(open("qcd_reweighted_ff_{}_{}.sav".format(channel,year), 'rb'))
-  #qcd_out = qcd_reweighter.predict_weights(new_df,weights)
-  #df.loc[:,"wt_ff_reweight_qcd_1"] = qcd_out*norm[channel][year]["qcd"]
-  
-  #ttbar_reweighter = pickle.load(open("ttbar_reweighted_ff_{}_{}.sav".format(channel,year), 'rb'))
-  #ttbar_out = ttbar_reweighter.predict_weights(new_df,weights)
-  #df.loc[:,"wt_ff_reweight_ttbar_1"] = ttbar_out*norm[channel][year]["qcd"]
-  
-  to_root(df, file_name.replace(".root","_"+str(j)+".root"), key='ntuple')
-  
-  del df, new_df, wjets_reweighter, total_weights, small_tree
-  j += 1
+      #wjets_reweighter = pickle.load(open("BDTs/wjets_reweighted_ff_{}_{}.sav".format(args.channel,args.year), 'rb'))
+      #wjets_out = wjets_reweighter.predict_weights(new_df,total_weights)
+      #df.loc[:,"wt_ff_reweight_wjets_1"] = wjets_out*norm_dict[args.channel][args.year]["wjets"]
+      
+      #qcd_reweighter = pickle.load(open("BDTs/qcd_reweighted_ff_{}_{}.sav".format(args.channel,args.year), 'rb'))
+      #qcd_out = qcd_reweighter.predict_weights(new_df,weights)
+      #df.loc[:,"wt_ff_reweight_qcd_1"] = qcd_out*norm_dict[args.channel][args.year]["qcd"]
+      
+      #ttbar_reweighter = pickle.load(open("BDTs/ttbar_reweighted_ff_{}_{}.sav".format(args.channel,args.year), 'rb'))
+      #ttbar_out = ttbar_reweighter.predict_weights(new_df,weights)
+      #df.loc[:,"wt_ff_reweight_ttbar_1"] = ttbar_out*norm_dict[args.channel][args.year]["qcd"]
+    
+    #elif channel == "tt":
+
+      #qcd_sublead_reweighter = pickle.load(open("BDTs/qcd_sublead_reweighted_ff_{}_{}.sav".format(args.channel,args.year), 'rb'))
+      #qcd_sublead_out = qcd_sublead_reweighter.predict_weights(new_df,weights)
+      #df.loc[:,"wt_ff_reweight_qcd_2"] = qcd_sublead_out*norm_dict[args.channel][args.year]["sublead"]
+
+      #qcd_lead_reweighter = pickle.load(open("BDTs/qcd_lead_reweighted_ff_{}_{}.sav".format(args.channel,args.year), 'rb'))
+      #qcd_lead_out = qcd_lead_reweighter.predict_weights(new_df,weights)
+      #df.loc[:,"wt_ff_reweight_qcd_1"] = qcd_lead_out*norm_dict[args.channel][args.year]["lead"]
+    
+    to_root(df, args.output_location+'/'+args.filename.replace(".root","_"+str(k)+".root"), key='ntuple')
+    
+    del df, new_df, wjets_reweighter, total_weights, small_tree
+  k += 1
+print "Finished processing"
---
+++ scripts/batch_add_ff_reweight_to_trees.py
@@ -0,0 +1,52 @@
+import ROOT
+import os
+from UserCode.ReweightedFakeFactors.ff_ml_tools import CreateBatchJob,SubmitBatchJob
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input_location',help= 'Name of input location (not including file name)', default='/vols/cms/gu18/Offline/output/MSSM/mssm_2017_v10')
+parser.add_argument('--output_location',help= 'Name of output location (not including file name)', default='/vols/cms/gu18/Offline/output/MSSM/reweight_ff_2017')
+parser.add_argument("--hadd", action='store_true',help="Hadd files after adding weights")
+args = parser.parse_args()
+
+loc = args.input_location
+newloc = args.output_location
+
+splitting = 100000 
+
+cmssw_base = os.getcwd().replace('src/UserCode/ReweightedFakeFactors','')
+
+for file_name in os.listdir(loc):
+  if ".root" in file_name:
+    if "_et_" in file_name:
+      channel = "et"
+    elif "_mt_" in file_name:
+      channel = "mt"
+    elif "_tt_" in file_name:
+      channel = "tt"
+
+    if "_2016" in file_name:
+      year = "2016"
+    elif "_2017" in file_name:
+      year = "2017"
+    elif "_2018" in file_name:
+      year = "2018"
+
+
+    if not args.hadd:
+      f = ROOT.TFile(loc+'/'+file_name,"READ")
+      t = f.Get("ntuple")
+      ent = t.GetEntries()
+      splits = ((ent - (ent%splitting))/splitting) + 1
+
+      for i in range(0,splits):
+        cmd = "python scripts/add_ff_reweight_to_trees.py --input_location=%(loc)s --output_location=%(newloc)s --filename=%(file_name)s --channel=%(channel)s --year=%(year)s --splitting=%(splitting)i --offset=%(i)i" % vars()
+        CreateBatchJob('jobs/'+file_name.replace('.root','_'+str(i)+'.sh'),cmssw_base,[cmd])
+        SubmitBatchJob('jobs/'+file_name.replace('.root','_'+str(i)+'.sh'),time=180,memory=24,cores=1)
+
+     else:
+       os.system("hadd -f {}/{} {}/{}".format(newloc,filename,newloc,filename.replace(".root","_*")))
+       os.system("rm {}/{}".format(newloc,filename.replace(".root","_*")))
+
+
+     
